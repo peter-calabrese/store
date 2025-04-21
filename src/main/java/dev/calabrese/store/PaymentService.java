e72342dca3214e4cd516043720bdfa51b475745e
Peter Calabrese <peter@calabrese.dev>
2025-04-21 09:37:10 -0400

    add order services

---
+++ src/main/java/dev/calabrese/store/PaymentService.java
@@ -0,0 +1,5 @@
+package dev.calabrese.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
---
+++ src/main/java/dev/calabrese/store/StripePaymentService.java
@@ -0,0 +1,10 @@
+package dev.calabrese.store;
+
+public class StripePaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount){
+        System.out.println("STRIPE");
+        System.out.println("Amount: " + amount);
+    }
+}
+
